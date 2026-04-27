da761d2465c4b75d0f091a02f85760286006131f
Theo <theo@cyberdyne.ai>
2026-04-27 15:57:09 +0000

    fix: buffer/global polyfills + deps for browser runtime

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+import { Buffer } from 'buffer';
+window.Buffer = window.Buffer || Buffer;
+window.global = window.global || window;
+window.process = window.process || { env: {} };
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
